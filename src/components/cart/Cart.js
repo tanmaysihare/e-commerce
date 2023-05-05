db8143f329683a6e9d6a3a4da3e66bfe7923fa4a
tanmaysihare <tanmaysihare@gmail.com>
2023-05-05 13:55:57 +0530

    cart

---
+++ src/components/cart/Cart.js
@@ -0,0 +1,45 @@
+import React,{ useState } from "react";
+import { Button, Alert } from "react-bootstrap";
+
+
+const Cart = () => {
+  const [show, setShow] = useState(true);
+
+  const cartElements = [
+    {
+    title: 'Colors',
+    price: 100,
+    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
+    quantity: 2,
+    },
+    {
+    title: 'Black and white Colors',
+    price: 50,
+    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
+    quantity: 3,
+    },
+    {
+    title: 'Yellow and Black Colors',
+    price: 70,
+    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
+    quantity: 1,
+    }
+    ]
+  return (
+    <>
+      <Alert show={show} variant="success">
+        <Alert.Heading>IN YOUR CART</Alert.Heading>
+      {cartElements}
+        <hr />
+        <div className="d-flex justify-content-end">
+          <Button onClick={() => setShow(false)} variant="outline-success">
+            Close me y'all!
+          </Button>
+        </div>
+      </Alert>
+
+      {!show && <Button onClick={() => setShow(true)}>Show Alert</Button>}
+    </>
+  );
+}
+export default Cart;
